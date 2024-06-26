50c8961ad549cfdad10d179ad5a46e1568f6dd50
Arthur Sahakyan <artursahakyan262@gmail.com>
2024-06-26 22:30:40 +0400

    up user service

--- services/user-service/src/main.ts
+++ services/user-service/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL],
+      queue: 'user_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000, () =>
+    console.log('User Service is listening at port 3000'),
+  );
 }
+
 bootstrap();
